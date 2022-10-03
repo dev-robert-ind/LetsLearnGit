a62be251b3d79ecfbaaf0105dcb162e2ba42b594
dev Robert India <alataf.dce.cs@outlook.com>
2022-10-04 01:59:48 +0530

    add implementation of Difference.java

--- Difference.cpp
+++ Difference.cpp
@@ -3,6 +3,13 @@ using namespace std;
 
 int main(){
 
+    
+    int num1=50;
+    int num2=40;
+
+    int differenceOfNumber=num1-num2;
+    cout<<"difference of two number is"<<differenceOfNumber<<endl;
+
 
 
 
