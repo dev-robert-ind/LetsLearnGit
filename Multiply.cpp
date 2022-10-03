a1a0c7c06fb6e02ee0398be11f981fd3c192c2d0
dev Robert India <alataf.dce.cs@outlook.com>
2022-10-03 23:18:42 +0530

    added Multiply.cpp

---
+++ Multiply.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int num1=10;
+    int num2=40;
+
+    int productOfNumber=num1*num2;
+    cout<<"Product of two number is"<<productOfNumber<<endl;
+
+}
